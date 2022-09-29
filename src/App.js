c593493715442c96b88310cecac50dec039ed4db
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-09-29 09:40:15 -0700

    deleted react logos, added robins logo and tested on page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.jpeg';
 
 function App() {
   return (
     <div className="App">
       Robin's plumbing
+      <img src={logo} />
     </div>
   );
 }
